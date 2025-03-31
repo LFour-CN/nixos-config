ddfc74e7fd653064942a26d78e6d691c6aec6c65
LFour <63118374+LFour-CN@users.noreply.github.com>
2025-03-31 22:56:56 +0800

    Add files via upload

---
+++ config/hypr/conf/hyprpaper.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+  
+  # Enable Hyprpaper on HDMI-A-1
+  hyprpaper
---
+++ config/swww/swww-daemon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+  #Run SWWW Daemon
+  swww-daemon
+
+  # Enable swww on HDMI-A-1
+  swww img /home/nixos/wallpapers/1743177186425.gif -o HDMI-A-1
